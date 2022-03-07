67b2fac8e5ac2b0a87cdf3e5ef43ed870ee7d6a4
Thunderbots <spectrumthunderbots7167@gmail.com>
2022-03-07 13:18:12 -0800

    Instantiate controller, motor groups

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,10 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
+
+//imports
+
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -12,4 +14,18 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    
+    //controller stuff
+    public final static int controllerPort = 0;
+
+    //PWM port locations 
+
+    //Drivetrain controller pwm ports (on RoboRIO)
+    public final static int pwmLeftFront = 0;
+    public final static int pwmLeftBack = 1; 
+    public final static int pwmRightFront = 2; 
+    public final static int pwmRightBack = 3; 
+
+
+}
